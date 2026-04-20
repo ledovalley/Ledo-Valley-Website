43666a6bb2a0c62f69c087ed57ce42d5e3bd3a00
Monish Ranjan <monishranjan9@gmail.com>
2026-04-20 13:07:15 +0530

    feat: integrate Meta Pixel tracking script and noscript fallback in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import InitialLoader from "@/components/layout/InitialLoader";
 import ServerWakingUpOverlay from "@/components/layout/ServerWakingUpOverlay";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -131,6 +132,22 @@ export default function RootLayout({
       gtag('config', 'G-WF1GGY11SE');
     `}
           </Script>
+
+          {/* Meta Pixel Code */}
+          <Script id="facebook-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1073788569141705');
+              fbq('track', 'PageView');
+            `}
+          </Script>
         </>
         <SpeedInsights />
         <Analytics />
@@ -140,6 +157,18 @@ export default function RootLayout({
               <UIProvider>
                 <Toaster position="top-right" richColors />
                 <ServerWakingUpOverlay />
+
+                {/* Meta Pixel Noscript Fallback */}
+                <noscript>
+                  <Image
+                    height="1"
+                    width="1"
+                    style={{ display: "none" }}
+                    src="https://www.facebook.com/tr?id=1073788569141705&ev=PageView&noscript=1"
+                    alt=""
+                  />
+                </noscript>
+
                 <AppShell>{children}</AppShell>
                 <Footer />
               </UIProvider>
